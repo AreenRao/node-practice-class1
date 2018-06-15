ae4d67e54356806f1a2d6369ab87f1ba1a35de22
AreenRao <anujdahiya1340@gmail.com>
2018-06-15 20:02:26 +0530

    using callback and error handling

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-var react = require('./reactangle');
+var rect = require('./reactangle');
 
-function calculateParameterAndArea(x, y) {
-    console.log("area is: ", react.area(x, y));
-    console.log("parameter is: ", react.parameter(x, y));
+function calculateParameterAndArea(l, b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message); // because we passed error as 'new Error' object. 
+	    }
+        else {
+            // we don't need to pass the parameters here because
+            // we have passed it in above 'rect()' function as 'l' & 'b'.
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    // this statement will get printed before the result of callbacks because we have used
+    // 'setTimeot' on callbacks in 'reactangle' module. 
+    console.log("This statement after the call to rect()");
 }
 
 calculateParameterAndArea(1, 2);
 calculateParameterAndArea(2, 1);
+calculateParameterAndArea(-2, 1);
--- reactangle.js
+++ reactangle.js
@@ -1,2 +1,26 @@
-module.exports.area = (x, y) => (x*y);
-module.exports.parameter = (x, y) => 2*(x+y);
+// in this commit we are using a whole function with callback as 'export'
+// to use in other modules. before this commit we were using 
+// function with parameters only.
+module.exports = (x,y,callback) => {
+    if (x <= 0 || y <= 0)
+        setTimeout(() => 
+            callback(new Error("Rectangle dimensions should be greater than zero: l = "
+                + x + ", and b = " + y), 
+            null),
+            2000);
+    else
+        setTimeout(() => 
+            callback(null, {
+                // here we used 'clousers' which javascript supports
+                // it means inner function can use the parameters of 
+                // outer function even after the completion of 
+                // outer function.
+                perimeter: () => (2*(x+y)), 
+                area:() => (x*y)
+            }), 
+            2000);
+}
+/*
+    we are using these 'callbacks' with 'setTimeout' because it takes time to 
+    recieve data from database so we don't want to write data before recieving the inputs.
+*/
